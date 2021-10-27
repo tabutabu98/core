4f46344301de1627705f0b4ca0eac8fc24799113
syc <songyechan.kr@gmail.com>
2021-10-27 23:39:42 +0900

    feat 컨테이너에 등록된 모든 빈 조회 @syc

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -16,6 +16,7 @@ public class OrderApp {
 //        MemberService memberService = appConfig.memberService();
 //        OrderService orderService = appConfig.orderService();
 
+        // ApplicationContext -> 스프링 컨테이너, 인터페이스
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
