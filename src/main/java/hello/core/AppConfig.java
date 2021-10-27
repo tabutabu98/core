0d7f4d17b6f06e5e460a82ce5e428423866c2b99
syc <songyechan.kr@gmail.com>
2021-10-27 23:00:39 +0900

    feat 스프링으로 전환하기 @syc

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,24 +8,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     // 생성자 주입
+    @Bean   // Bean -> 스프링컨테이너에 등록
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
     // 회원 저장소 : 메모리멤버리포지토리
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     // 할인 정책 : 픽스이스카운트폴리스
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
