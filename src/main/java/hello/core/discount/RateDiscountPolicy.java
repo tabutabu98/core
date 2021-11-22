d49e4149777996624462f92532dd3bf9165765f5
syc <syc@partdb.com>
2021-11-22 12:44:40 +0900

    feat 조회 빈이 2개 이상 - 문제 @syc

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -7,12 +7,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class RateDiscountPolicy implements DiscountPolicy{
 
-    private int dicountPercent = 10;
+    private int discountPercent = 10;
 
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
-            return price * dicountPercent / 100;
+            return price * discountPercent / 100;
         } else {
             return 0;
         }
